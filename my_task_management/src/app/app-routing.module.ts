274719112151c07545a4e98c3678ced920ee90e3
DESKTOP-MVV37G8\ADMIN <minhnase173546@fpt.edu.vn>
2025-11-04 22:10:52 +0700

    feat: (add) root  angular

---
+++ my_task_management/src/app/app-routing.module.ts
@@ -0,0 +1,6 @@
+// const routes: Routes = [
+//   { path: '', loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule) },
+//   { path: 'users', loadChildren: () => import('./features/users/users.module').then(m => m.UsersModule) },
+//   { path: 'auth', loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule) },
+//   { path: '**', redirectTo: '' }
+// ];
---
+++ my_task_management/src/app/app.config.ts
@@ -0,0 +1,19 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection, isDevMode } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+]
+};
